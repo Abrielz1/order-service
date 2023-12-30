47ed00ecae652fa602caad8ed41f58f98f47aadd
Abriel <abrielz11@gmail.com>
2023-12-30 11:59:51 +0300

    controller were added

--- src/main/java/com/example/orderservice/service/KafkaMessageService.java
+++ src/main/java/com/example/orderservice/service/KafkaMessageService.java
@@ -4,7 +4,6 @@ import com.example.orderservice.model.KafkaMessage;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class KafkaMessageService {
@@ -13,11 +12,5 @@ public class KafkaMessageService {
 
     public void add(KafkaMessage message) {
         messages.add(message);
-    }
-
-    public Optional<KafkaMessage> getById(Long id) {
-        return messages.stream()
-                .filter(i -> i.getId().equals(id))
-                .findFirst();
     }
 }
--- src/main/java/com/example/orderservice/web/controller/KafkaController.java
+++ src/main/java/com/example/orderservice/web/controller/KafkaController.java
@@ -1,7 +1,12 @@
 package com.example.orderservice.web.controller;
 
+import com.example.orderservice.model.KafkaMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class KafkaController {
 
-    //TODO:
-    // PostMapping("/send")
-    // @ResponseStatus(HttpStatus.OK)
-    // public String sendMessage(@RequestBody KafkaMessage message) {
-    // kafkaTemplate.send(topicName, message);
-    // return "Message were send to kafka";
-    // }
+    @Value("${app.kafka.kafkaMessageTopic")
+    private String topicName;
+
+    private final KafkaTemplate<String, KafkaMessage> kafkaTemplate;
+
+    @PostMapping("/send")
+     @ResponseStatus(HttpStatus.OK)
+     public String sendMessage(@RequestBody KafkaMessage message) {
+     kafkaTemplate.send(topicName, message);
+     return "Message were send to kafka";
+     }
 }
