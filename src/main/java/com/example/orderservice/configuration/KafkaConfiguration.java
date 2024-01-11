1bb6215ad7182841041fe35f26c3cce48eae22f5
Abriel <abrielz11@gmail.com>
2024-01-12 00:12:02 +0300

    poet were changed in yml file to 8080

--- src/main/java/com/example/orderservice/configuration/KafkaConfiguration.java
+++ src/main/java/com/example/orderservice/configuration/KafkaConfiguration.java
@@ -9,6 +9,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
@@ -21,6 +22,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@EnableKafka
 public class KafkaConfiguration {
 
     @Value("${spring.kafka.bootstrap-servers}")
